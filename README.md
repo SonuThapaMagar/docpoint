5500c79720052f39260113b10d9d108a532a913b
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-27 20:56:36 +0545

    Pages added and routed

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Docpoint
+# Docpoint
 This is a simple doctor's appointment booking system where the system allows patients to book appointments with doctors, while doctors can manage their schedule.
 
-# Features
+## Features
 - Doctor's appointment
 - Checkout
--- docpoint/src/App.jsx
+++ docpoint/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-10'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/myProfile" element={<MyProfile />} />
+        <Route path="/myAppointments" element={<MyAppointments />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
